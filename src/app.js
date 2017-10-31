93576d747c7edf98e21f6b79a989f65d9874bbcd
Allen Schwartz <allen@Jacobs-MacBook-Air.local>
2017-10-31 10:37:26 -0400

    first commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
-import DecisionApp from './components/DecisionApp';
 import './styles/styles.scss';
 
-ReactDOM.render(<DecisionApp />, document.getElementById('app'));
+ReactDOM.render(<p>Boilerplate</p>, document.getElementById('app'));
